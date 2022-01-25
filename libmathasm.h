34a358f283a7068f1a27a822ba39164291c59ec6
sxprz <derian.pljakic@gmail.com>
2022-01-25 14:30:27 +0100

    Fix header file and add factorial function

--- libmathasm.h
+++ libmathasm.h
@@ -1,6 +1,9 @@
 #ifndef LIBMATHASM_H
 #define LIBMATHASM_H
 
+// TODO: add function declarations
+// Calculate the factorial of n
+unsigned long fac(int n);
 
 
-#endif LIBMATHASM_H /* libmathasm.h */
+#endif /* libmathasm.h */
